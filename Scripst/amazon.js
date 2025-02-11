38541d2fafccb24c6b13a39e9ed06b3b3b3bd323
Constant Vlad <vladok21381@gmail.com>
2025-02-11 22:08:32 +0100

    Change the button

--- Scripst/amazon.js
+++ Scripst/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -42,7 +42,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -60,28 +60,43 @@ productGrid.innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+     const {productId} = button.dataset;
+     let matchingItem;
 
-      let matchingItem;
+     const selectQuantity = document.querySelector(`.js-quantity-selector-${productId}`);
+     const quantity = Number(selectQuantity.value);
 
-      cart.forEach((item) => {
-        if(productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if(matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
+     cart.forEach((item) => {
+      if(productId === item.productId) {
+        matchingItem = item
       }
+     });
 
-      cart.forEach(() => {
-        
-      })
-      console.log(cart);
+     if(matchingItem) {
+      matchingItem.quantity += quantity;
+     } else {
+       cart.push({
+         productId,
+         quantity
+       });
+     }
+
+     let cartQuantity = 0;
+
+     cart.forEach((item) => {
+      cartQuantity += item.quantity;
+     });
+
+     document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+     
+     const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+     addedMessage.classList.add('added-to-cart-visible');
+
+
+    setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+     },1000);
+  
     });
   });
--- data/products.js
+++ data/products.js
@@ -656,6 +656,33 @@ const products = [
       "apparel",
       "mens"
     ]
-  }
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d3",
+    image: "images/products/umbrella.jpg",
+    name: "A big big Umbrella",
+    rating: {
+      stars: 5,
+      count: 130
+    },
+    priceCents: 3000,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d12",
+    image: "images/products/backpack.jpg",
+    name: "A small Backpack",
+    rating: {
+      stars: 1,
+      count: 1
+    },
+    priceCents: 9095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
 ];
-console.log(products);
