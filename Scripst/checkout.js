d94ebbdb253561dcebd5109ad7dd17dd941ce9e0
Constant Vlad <vladok21381@gmail.com>
2025-03-10 22:53:32 +0100

    Another change

--- Scripst/checkout.js
+++ Scripst/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calcuateCartCuantity } from "../data/cart.js";
+import { cart, removeFromCart, calcuateCartCuantity, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -35,7 +35,7 @@ cart.forEach((cartItem) => {
           </div>
           <div class="product-quantity">
             <span>
-              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+              Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
@@ -128,12 +128,27 @@ document.querySelectorAll('.js-delete-link')
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
   
+        const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+        const newQuantity = Number(quantityInput.value);
+
+        if(newQuantity < 0 || newQuantity >= 1000) {
+          alert(' Quantity must be at least 0 and less than 1000');
+          return;
+        }
+
+        updateQuantity(productId,newQuantity);
+
         const container = document.querySelector(
           `.js-cart-item-container-${productId}`
         );
         container.classList.remove('is-editing-quantity');
-        const quantityInput = document.querySelector(`.js-quntity-input-${productId}`);
-        const newQuantity = Number(quantityInput.value);
+
+        const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
+
+        quantityLabel.innerHTML = newQuantity;
+        
+
+        updateCartQuantity();
       });
     })
 
