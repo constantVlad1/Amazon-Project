93663dc2f74bb6ce4d146b66310adc475d6646ba
Constant Vlad <vladok21381@gmail.com>
2025-07-28 21:26:29 +0200

    Convert products into classes

--- Scripst/checkout.js
+++ Scripst/checkout.js
@@ -1,7 +1,7 @@
 import {renderCheckoutHeader} from "./checkout/checkoutHeader.js"
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymantSummary.js";
-import '../data/cart-class.js'
+//import '../data/cart-class.js'
 
 renderCheckoutHeader();
 renderOrderSummary();
