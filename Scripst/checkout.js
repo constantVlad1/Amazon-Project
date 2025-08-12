b40b0f218df83b575ba66a34ad7124fb6f2be9b5
Constant Vlad <vladok21381@gmail.com>
2025-08-12 21:18:16 +0200

    Add backend practice file

--- Scripst/checkout.js
+++ Scripst/checkout.js
@@ -2,6 +2,7 @@ import {renderCheckoutHeader} from "./checkout/checkoutHeader.js"
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymantSummary.js";
 //import '../data/cart-class.js'
+import '../data/backend_practice.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
