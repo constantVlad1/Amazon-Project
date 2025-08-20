da6ad1cabaf7588bcd889f40d58407241012a28f
Constant Vlad <vladok21381@gmail.com>
2025-08-20 19:28:49 +0200

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -57,6 +57,38 @@ class Clothing extends Product{
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = 
+  fetch('https://supersimplebackend.dev/products')
+  .then((response) => {
+    return response.json()
+  })
+  .then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+     return new Product(productDetails);
+    });
+
+    console.log('load products')
+
+    
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
+
+
+  return promise;
+}
+
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step')
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -71,6 +103,10 @@ export function loadProducts(fun) {
     
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
